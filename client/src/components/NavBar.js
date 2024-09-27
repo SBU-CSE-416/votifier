17f4a4c05af85c80f03631aed91621c1a2801532
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2024-09-26 21:34:26 -0400

    updated nav bar button alignments

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,11 +4,15 @@ export default function NavBar() {
     return (
         <div className='nav-container'>
             <header className='navbar'>
-                <img id="sbu-logo-img" src="/sbu_round_logo.png" alt="sbu logo"></img>
-                <p>Votifier</p>
-                <p>Map</p>
-                <p>Data</p>
-                <p>Credits</p>
+                <div style={{display:"flex", alignItems:"center"}}>
+                    <img id="sbu-logo-img" src="/sbu_round_logo.png" alt="sbu logo"></img>
+                    <p id='home-btn' className='text-btn'>Votifier</p>
+                </div>
+                <div style={{display:"flex", justifyContent:"flex-end"}}>
+                    <p id="map-btn" className='text-btn'>Map</p>
+                    <p id="data-btn" className='text-btn'>Data</p>
+                    <p id="credits-btn" className='text-btn'>Credits</p>
+                </div>
             </header>
         </div>
     );
