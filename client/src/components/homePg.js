7b76d51f9afa4e0799cbf9c90cb4ef9a3f2a3f16
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2024-09-26 20:32:21 -0400

    sticky nav bar added, votifier and home components added, added folder organization

---
+++ client/src/components/homePg.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import NavBar from "./navBar.js";
+
+export default function HomePg() {
+    return (
+        <div>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+            <p>Testing</p>
+
+
+        </div>
+    );
+}
