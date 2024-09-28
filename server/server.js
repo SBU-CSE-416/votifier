3a2a8cad26604a0429a2b7dd205961a31c98b01c
tonybuii2003 <longphibui1993@gmail.com>
2024-09-28 18:29:33 -0400

    add server changes

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require("cors");
+const connectDB = require("./config/db");
+const mongoose = require("mongoose");
+const port = 8000;
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    credentials: true, 
+  };
+  
+connectDB();
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    console.log('Request Body:', req.body);
+    next();
+});
+const server = app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+const shutdown = async () => {
+    console.log('\nServer is shutting down...');
+
+    
+    server.close(async () => {
+        try {
+            await mongoose.connection.close();
+            console.log('Server closed. Database instance disconnected');
+        } catch (error) {
+            console.error('Error during database disconnection:', error);
+        }
+        process.exit(0);
+    });
+};
+
+process.on('SIGINT', shutdown);  
+process.on('SIGTERM', shutdown); 
