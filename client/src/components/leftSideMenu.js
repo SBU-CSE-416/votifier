55162f8b2d6299fc99cdc98a627239533992ff41
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2024-10-02 19:10:42 -0400

    removed graphs and demographics from side nav bar

--- client/src/components/leftSideMenu.js
+++ client/src/components/leftSideMenu.js
@@ -8,48 +8,13 @@ export default function LeftSideMenu(){
         southCarolina: false,
     });
 
-    const [ isGraphsOpen, setGraphsOpen ] = useState(false);
-    const [ selectedGraphs, setSelectedGraphs ] = useState({
-        bar: false,
-        histogram: false,
-    });
-
-    const [ isDemographicsOpen, setDemographicsOpen ] = useState(false);
-    const [ selectedDemographics, setSelectedDemographics ] = useState({
-        income: false,
-        race: false,
-        age: false,
-    });
-
     const toggleStateDropdown = () => {
         setStatesOpen(!isStatesOpen);
-    };
-
-    const toggleGraphsDropdown = () => {
-        setGraphsOpen(!isGraphsOpen);
-    };
-
-    const toggleDemographicsDropdown = () => {
-        setDemographicsOpen(!isDemographicsOpen);
     };
 
     const handleStatesCheckChange = (event) => {
         setSelectedStates({
             ...selectedStates, 
-            [event.target.name]: event.target.checked
-        });
-    };
-
-    const handleGraphsCheckChange = (event) => {
-        setSelectedGraphs({
-            ...selectedGraphs, 
-            [event.target.name]: event.target.checked
-        });
-    };
-
-    const handleDemographicsCheckChange = (event) => {
-        setSelectedDemographics({
-            ...selectedDemographics, 
             [event.target.name]: event.target.checked
         });
     };
@@ -83,68 +48,6 @@ export default function LeftSideMenu(){
                 )}
             </div>
 
-            {/* Graphs Dropdown Menu*/}
-            <div className="dropdown-category">
-                <p onClick={toggleGraphsDropdown} className="dropdown-title">{ isGraphsOpen ? "v Graphs" : ">Graphs"}</p>
-                {isGraphsOpen && (
-                    <div className="dropdown-content">
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="bar"
-                                checked={selectedGraphs.bar}
-                                onChange={handleGraphsCheckChange}
-                            />
-                            Bar
-                        </label>
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="histogram"
-                                checked={selectedGraphs.histogram}
-                                onChange={handleGraphsCheckChange}
-                            />
-                            Histogram
-                        </label>
-                    </div>
-                )}
-            </div>
-
-            {/* Demographics Dropdown Menu */}
-            <div className="dropdown-category">
-                <p onClick={toggleDemographicsDropdown} className="dropdown-title">{isDemographicsOpen ? "v Demographics" : ">Demographics"}</p>
-                {isDemographicsOpen && (
-                    <div className="dropdown-content">
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="income"
-                                checked={selectedDemographics.income}
-                                onChange={handleDemographicsCheckChange}
-                            />
-                            Income
-                        </label>
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="race"
-                                checked={selectedDemographics.race}
-                                onChange={handleDemographicsCheckChange}
-                            />
-                            Race
-                        </label>
-                        <label>
-                            <input
-                                type="checkbox"
-                                name="age"
-                                checked={selectedDemographics.age}
-                                onChange={handleDemographicsCheckChange}
-                            />
-                            Age
-                        </label>
-                    </div>
-                )}
-            </div>
         </div>
     );
 }
