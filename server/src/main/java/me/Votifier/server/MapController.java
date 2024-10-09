ea7177a8840633f2461eea51f0495bf42faff6df
tonybuii2003 <longphibui1993@gmail.com>
2024-10-09 19:47:30 -0400

    add routes to states

--- server/src/main/java/me/Votifier/server/MapController.java
+++ server/src/main/java/me/Votifier/server/MapController.java
@@ -1,15 +1,41 @@
 package me.Votifier.server;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class MapController {
 
-    @GetMapping
-    public String getMapping() {
-        return "Map route response";
+    private static final Map<Integer, String> stateIdToName = Map.of(
+            24, "maryland",
+            45, "south_carolina"
+    );
+    @GetMapping("/{stateid}")
+    public ResponseEntity<Resource> getState(@PathVariable("stateid") int stateId) {
+        String stateName = stateIdToName.get(stateId);
+        if (stateName == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            Path filePath = Paths.get("data/states/" + stateName + "/geodata/" + stateName + "_state.geojson");
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return ResponseEntity.ok(resource);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (MalformedURLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
