fa64b5fc715190cba5635c32703ed2ce666b7616
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-11-17 15:21:24 -0500

    update stateid to fips_code in controller

--- server_js/controllers/mapController.js
+++ server_js/controllers/mapController.js
@@ -1,43 +1,43 @@
-const path = require('path');
+const path = require("path");
 
 const stateIdToName = {
-    24: "maryland",
-    45: "south_carolina",
+  24: "maryland",
+  45: "south_carolina",
 };
 
-function getState(req, res) {    
-    const stateId = req.params.stateid; 
-    try {
-        const stateName = stateIdToName[stateId];
-        const filePath = `../data/states/${stateName}/geodata/${stateName}_state.geojson`;
-        newPath = path.join(__dirname, filePath);
+function getState(req, res) {
+  const stateId = req.params.stateid;
+  try {
+    const stateName = stateIdToName[stateId];
+    const filePath = `../data/states/${stateName}/geodata/${stateName}_state.geojson`;
+    newPath = path.join(__dirname, filePath);
 
-        res.sendFile(newPath, (err) => {
-            if (err) {
-              res.status(500).send("File not found");
-            }
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    res.sendFile(newPath, (err) => {
+      if (err) {
+        res.status(500).send("File not found");
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-function getDistricts(req, res){
-    const stateId = req.params.stateid; 
-    try {
-        const stateName = stateIdToName[stateId]
+function getDistricts(req, res) {
+  const fips_code = req.params.fips_code;
+  try {
+    const stateName = fipsCodeToName[fips_code];
 
-        const filePath = `../data/states/${stateName}/geodata/${stateName}_cds.geojson`;
-        newPath = path.join(__dirname, filePath);
+    const filePath = `../data/states/${stateName}/geodata/${stateName}_cds.geojson`;
+    newPath = path.join(__dirname, filePath);
 
-        res.sendFile(newPath, (err) => {
-            if (err) {
-              res.status(500).send("File not found");
-            }
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    res.sendFile(newPath, (err) => {
+      if (err) {
+        res.status(500).send("File not found");
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-module.exports = { getState, getDistricts }
+module.exports = { getState, getDistricts };
