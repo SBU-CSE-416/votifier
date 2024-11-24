a68656d07849ccdbcad9eec017dcbfd548268cfd
tonybuii2003 <longphibui1993@gmail.com>
2024-11-24 09:55:08 -0500

    detele checkstate

--- client/src/components/votifier.js
+++ client/src/components/votifier.js
@@ -1,34 +1,11 @@
 import { useState } from "react";
 import NavBar from "./NavBar";
 import LeftSideMenu from "./map and data/LeftSideMenu";
-import HomePg from "./HomePg";
 import MapPg from "./map and data/MapPg";
-import CreditPg from "./CreditsPg";
 
 export default function Votifier() {
   const [pg, setPage] = useState("home");
 
-  const checkState = () => {
-    if (pg === "home") {
-      return (
-        <>
-          <HomePg></HomePg>
-        </>
-      );
-    } else if (pg === "map") {
-      return (
-        <>
-          <MapPg></MapPg>
-        </>
-      );
-    } else if (pg === "credits") {
-      return (
-        <>
-          <CreditPg></CreditPg>
-        </>
-      );
-    }
-  };
 
   return (
     <section style={{ display: "flex", flexDirection: "column" }}>
