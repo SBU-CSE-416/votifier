c9ef848cbd7fbbd711e0bcb97961a2dee1c5de8c
vitaliy <ryanstusalitusEPK@gmail.com>
2024-12-04 08:59:58 -0500

    Seawulf preprocessing refactoring

--- server/data/seawulf/gerrychain_mggg.py
+++ server/data/seawulf/gerrychain_mggg.py
@@ -1,23 +1,50 @@
+import argparse
+import json
+import random
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.readwrite import json_graph
+
 import gerrychain
-from gerrychain import (Partition, Graph, MarkovChain, Election, updaters, constraints, accept)
+from gerrychain import (
+   Partition, MarkovChain, Election, constraints, accept
+)
 from gerrychain.updaters import Tally, cut_edges
 from gerrychain.proposals import recom
 from gerrychain.tree import bipartition_tree
-from gerrychain.constraints import contiguous
-from functools import partial
-import argparse
-import json
-import networkx as nx
-from networkx.readwrite import json_graph
-import random
-import pandas as pd
-import matplotlib.pyplot as plt
 
-def calculate_district_election_winner():
-   return
+from functools import partial
+from seawulf_constants import *
+
+def race_percentage_updater(selected_racial_group):
+    def updater(partition):
+        percentages = {}
+        for district, nodes in partition.parts.items():
+            total_population = sum(partition.graph.nodes[node]["TOT_POP22"] for node in nodes)
+            race_population = sum(partition.graph.nodes[node][selected_racial_group] for node in nodes)
+            percentages[district] = (race_population / total_population) * 100 if total_population > 0 else 0
+        return percentages
+    return updater
+
+def election_results_updater(partition):
+    results = {}
+    for district, nodes in partition.parts.items():
+        dem_votes = sum(partition.graph.nodes[node]["TOT_DEM"] for node in nodes)
+        rep_votes = sum(partition.graph.nodes[node]["TOT_REP"] for node in nodes)
+        winner = "Democratic" if dem_votes > rep_votes else "Republican"
+        results[district] = {
+            "Democratic": dem_votes,
+            "Republican": rep_votes,
+            "Winner": winner
+        }
+    return results
 
 def cut_edges_length(partition):
   return len(partition["cut_edges"])
+
+
 
 def generate_new_districting_plan(state_graph_json_file, plan_id, num_iterations, pop_constraint):
   random.seed(plan_id)
@@ -25,14 +52,23 @@ def generate_new_districting_plan(state_graph_json_file, plan_id, num_iterations
     precinct_graph_json = json.load(precinct_graph_file)
   precinct_graph = json_graph.node_link_graph(precinct_graph_json, edges="links")
 
-  # Set up the initial partition object
   elections = [
     Election("GOV_22", {"Democratic": "TOT_DEM", "Republican": "TOT_REP"})
   ]
 
+  racial_group_updater_white = race_percentage_updater(WHITE_POP_LABEL)
+  racial_group_updater_black = race_percentage_updater(BLACK_POP_LABEL)
+  racial_group_updater_asian = race_percentage_updater(ASIAN_POP_LABEL)
+  racial_group_updater_hispanic = race_percentage_updater(HISPANIC_POP_LABEL)
+
   all_updaters = {
       "population": Tally("TOT_POP22", alias="population"),
       "cut_edges": cut_edges,
+      "2022_gubernatorial_election_results": election_results_updater,
+      "racial_percentages_white": racial_group_updater_white,
+      "racial_percentages_black": racial_group_updater_black,
+      "racial_percentages_asian": racial_group_updater_asian,
+      "racial_percentages_hispanic": racial_group_updater_hispanic
   }
 
   election_updaters = {election.name: election for election in elections}
@@ -74,40 +110,36 @@ def generate_new_districting_plan(state_graph_json_file, plan_id, num_iterations
     total_steps=num_iterations
   )
 
-  election_data = pd.DataFrame(
-    [sorted(partition["GOV_22"].percents("Democratic"))
-    for partition in recom_chain.with_progress_bar()]
-  )
+  last_partition = None
+  for partition in recom_chain.with_progress_bar():
+     last_partition = partition
 
-  election_data.to_csv("last_partition.csv", index=False)
+  gubernatorial_election_results = last_partition["2022_gubernatorial_election_results"]
+  racial_results_white = last_partition["racial_percentages_white"]
+  racial_results_black = last_partition["racial_percentages_black"]
+  racial_results_asian = last_partition["racial_percentages_asian"]
+  racial_results_hispanic = last_partition["racial_percentages_hispanic"]
 
-  fig, ax = plt.subplots(figsize=(8, 6))
+  racial_groups_df = pd.DataFrame({
+    "White": racial_results_white,
+    "Black": racial_results_black,
+    "Asian": racial_results_asian,
+    "Hispanic": racial_results_hispanic
+  })
 
-  # Draw 50% line
-  ax.axhline(0.5, color="#cccccc")
-
-  # Draw boxplot
-  election_data.boxplot(ax=ax, positions=range(len(election_data.columns)))
-  # Draw initial plan's Democratic vote %s (.iloc[0] gives the first row)
-  plt.plot(election_data.iloc[0], "ro")
-
-  # Annotate
-  ax.set_title("Comparing the 2022 plan to an ensemble")
-  ax.set_ylabel("Democratic vote % (Governor 2022")
-  ax.set_xlabel("Sorted Districts")
-  ax.set_ylim(0, 1)
-  ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
-
-  plt.savefig('plotdemo.png')
+  summary_stats = racial_groups_df.describe()
+  print(summary_stats)
+  
+  election_results_df = pd.DataFrame.from_dict(gubernatorial_election_results, orient="index")
+  election_results_df.to_csv("results/plan_election_results.csv")
   return
 
 if __name__ == "__main__":
-    POP_CONSTRAINT = 0.1
+    POP_CONSTRAINT = 0.08
     arguments_parser = argparse.ArgumentParser()
     arguments_parser.add_argument('--state_graph_file', type=str, required=True)
     arguments_parser.add_argument('--plan_id', type=str, required=True)
     arguments_parser.add_argument('--iterations', type=int, required=True)
     args = arguments_parser.parse_args()
     generated_plan = generate_new_districting_plan(args.state_graph_file, args.plan_id, args.iterations, POP_CONSTRAINT)
-    # calculate_district_election_winner(generated_plan)
-    # Save plan to file path?
+    # Not yet implemented: Exporting the generated data
