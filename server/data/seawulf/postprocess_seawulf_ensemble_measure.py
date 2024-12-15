51e18e0dac84b119a9a54d0fe38213a0bf773f7e
vitaliy <ryanstusalitusEPK@gmail.com>
2024-12-15 15:31:23 -0500

    Finished seawulf postprocessing

--- server/data/seawulf/postprocess_seawulf_ensemble_measure.py
+++ server/data/seawulf/postprocess_seawulf_ensemble_measure.py
@@ -4,82 +4,116 @@ import json
 import numpy as np
 from seawulf_constants import *
 
-def postprocessing_process(ensemble_id, state_abbr, racial_group, plan_count):
-    # group_district_buckets = get_district_buckets(ensemble_id, racial_group, plan_count)
-    # group_district_buckets_list = list(group_district_buckets.items())
+def calculate_y_axis_ticks(min_val, max_val, starting_ticks):
+    # Adjust min_val and max_val to the nearest even numbers
+    if min_val % 2 != 0:
+        min_val -= 1  # Round down to the nearest even number
+    if max_val % 2 != 0:
+        max_val += 1  # Round up to the nearest even number
+    tick_range = max_val - min_val
+    if tick_range < starting_ticks:
+        starting_ticks = starting_ticks / 2
+    ticks = None
+    if tick_range < starting_ticks * 2:
+        tick_interval = 2
+        ticks = np.arange(min_val, max_val + tick_interval, tick_interval).tolist()
+        alt = 0 
+        while len(ticks) < starting_ticks:
+            if alt == 0 or alt == 3:
+                # Back half
+                last_index = len(ticks)-1
+                new_tick = ticks[last_index] + tick_interval
+                ticks.insert(last_index+1, new_tick)
+                if alt != 3:
+                    alt = 1
+            else:
+                # Front half
+                if ticks[0] - tick_interval < 0:
+                    alt = 3
+                else:
+                    new_tick = ticks[0] - tick_interval
+                    ticks.insert(0, new_tick)
+                    alt = 0
+    else:
+        tick_interval = int((max_val - min_val) / (starting_ticks))
+        if tick_interval == 0:
+            tick_interval = 1  # Avoid division by zero by defaulting to 1
+        ticks = np.arange(min_val, max_val + tick_interval, tick_interval).tolist()
+    return ticks
 
+def generate_outcome_buckets(total_districts):
+    # Generate all possible outcomes of wins for DEM and REP in a given number of districts
+    outcome_dict = {}
+    for dem_wins in range(total_districts + 1):
+        rep_wins = total_districts - dem_wins
+        key = f"{dem_wins}:{rep_wins}"
+        outcome_dict[key] = 0 
+    return outcome_dict
 
-    # congressional_districts_2022_filepath = f"{state_abbr.lower()}_congressional_districts_summary_seawulf.json"
-    # with open(congressional_districts_2022_filepath, 'r') as congressional_districts_2022_file:
-    #     districts_2022_json = json.load(congressional_districts_2022_file)
-    #     racial_group_key = racial_group.upper() + "_PERCENT"
-    #     result = {
-    #         str(item['CONG_DIST']).zfill(2): item[racial_group_key]
-    #         for item in districts_2022_json['data']
-    #     }
-    #     sorted_districts_2022_json = dict(sorted(result.items(), key=lambda x: x[1]))
-    #     sorted_districts_2022_list = list(sorted_districts_2022_json.items())
-    # group_box_and_whisker_data = get_box_and_whisker_data(group_district_buckets_list, racial_group, sorted_districts_2022_list)
-    # lowest_min = 101
-    # highest_max = -1
-    # for district, metrics in group_box_and_whisker_data.items():
-    #     # Update lowest_min if the current MIN is lower
-    #     if metrics['MIN'] < lowest_min:
-    #         lowest_min = metrics['MIN']
-    #     # Update highest_max if the current MAX is higher
-    #     if metrics['MAX'] > highest_max:
-    #         highest_max = metrics['MAX']
+def postprocessing_process(ensemble_id, state_abbr, plan_count):
+    buckets = None
+    district_count = 0
+    for plan_id in range(1, plan_count+1):
+        with open(f"results/ensemble_{ensemble_id}/plan_{plan_id}/district_election_results.json") as plan_election_results_file:
+            plan_election_results_json = json.load(plan_election_results_file)
+        if buckets is None:
+            district_count = len(plan_election_results_json)
+            buckets = generate_outcome_buckets(district_count)
+        rep_wins = 0
+        dem_wins = 0
+        for district, election_data in plan_election_results_json.items():
+            if election_data["WIN"] == "REP":
+                rep_wins += 1
+            else:
+                dem_wins += 1
+        split_str = f"{dem_wins}:{rep_wins}"
+        buckets[split_str] += 1
+    
+    max_val = 0
+    for bucket, value in buckets.items():
+        if value > max_val:
+            max_val = value
+    
+    chart_ensemble_id = ensemble_id
+    if ensemble_id == 3:
+        chart_ensemble_id = 1
+    elif ensemble_id == 4:
+        chart_ensemble_id = 2
 
+    state_name = None
 
-    # chart_ensemble_id = ensemble_id
-    # if ensemble_id == 3:
-    #     chart_ensemble_id = 1
-    # elif ensemble_id == 4:
-    #     chart_ensemble_id = 2
+    if state_abbr == "MD":
+        state_name = "Maryland"
+    elif state_abbr == "SC":
+        state_name = "South Carolina"
 
-    # state_name = None
+    yticks = calculate_y_axis_ticks(0, (max_val + 4), 10)
+    labels = {
+        "title" : f"Vote Splits Across Ensemble",
+        "subtitle": f"({state_name} - Ensemble {chart_ensemble_id}, {plan_count} Plans)",
+        "axis-x": "Win Ratios (Democratic Party:Republican Party)",
+        "axis-y": f"Outcome Frequency",
+        "axis-x-ticks" : list(buckets.keys()),
+        "axis-y-ticks": yticks,
+    }
 
-    # if state_abbr == "MD":
-    #     state_name = "Maryland"
-    # elif state_abbr == "SC":
-    #     state_name = "South Carolina"
-
-
-    # legend = {
-    #     "enacted_dots" : "Enacted Plan (2022)"
-    # }
-
-    # yticks = calculate_y_axis_ticks(lowest_min, highest_max)
-    # labels = {
-    #     "title" : f"{racial_group.capitalize()} Population Percent Across Ensemble",
-    #     "subtitle": f"({state_name} - Ensemble {chart_ensemble_id}, {plan_count} Plans)",
-    #     "axis-x": "Bucket Index",
-    #     "axis-y": f"{racial_group.capitalize()} Population Percent",
-    #     "axis-x-ticks" : [i for i in range(1, len(group_district_buckets.keys())+1)],
-    #     "axis-y-ticks": yticks,
-    #     "legend": legend
-    # }
-
-    # finalized_racial_whisker_json = {
-    #     "NAME": state_name,
-    #     "ENSEMBLE_ID": ensemble_id,
-    #     "TOT_PLANS": plan_count,
-    #     "RACIAL_GROUP": racial_group.upper(),
-    #     "labels" : labels,
-    #     "data": group_box_and_whisker_data
-    # }
-
-    # finalized_racial_whisker_json_str = json.dumps(finalized_racial_whisker_json, indent=4)
-    # finalized_path = f"results/ensemble_{ensemble_id}/box_and_whisker_results_{state_abbr}_racial_group_{racial_group.upper()}.json"
-    # with open(finalized_path, 'w') as json_file:
-    #     json_file.write(finalized_racial_whisker_json_str)
+    finalized_json = {
+        "NAME": state_name,
+        "ENSEMBLE_ID": ensemble_id,
+        "TOT_PLANS": plan_count,
+        "labels" : labels,
+        "data": buckets
+    }
+    finalized_json_str = json.dumps(finalized_json, indent=4)
+    finalized_path = f"results/ensemble_{ensemble_id}/vote_splits_{state_abbr}.json"
+    with open(finalized_path, 'w') as json_file:
+        json_file.write(finalized_json_str)
     return
 
 if __name__ == "__main__":
     arguments_parser = argparse.ArgumentParser()
     arguments_parser.add_argument('--ensemble_id', type=int, required=True)
     arguments_parser.add_argument('--state_abbr', type=str, required=True)
-    arguments_parser.add_argument('--racial_group', type=str, required=True)
     arguments_parser.add_argument('--plan_count', type=int, required=True)
     args = arguments_parser.parse_args()
-    postprocessing_process(args.ensemble_id, args.state_abbr, args.racial_group, args.plan_count)
+    postprocessing_process(args.ensemble_id, args.state_abbr, args.plan_count)
