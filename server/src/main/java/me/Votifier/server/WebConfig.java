e993d0a47c7fffd6e7f11ce364aba74f022f9b2e
tonybuii2003 <longphibui1993@gmail.com>
2024-12-23 23:03:34 -0500

    udpate CORS

--- server/src/main/java/me/Votifier/server/WebConfig.java
+++ server/src/main/java/me/Votifier/server/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://votifier-client-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
